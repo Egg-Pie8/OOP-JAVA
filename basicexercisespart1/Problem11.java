e94e3126c174c1c528c62ef66dd60d6e2b6b8473
Egg-Pie8 <riansalumayag@gmail.com>
2025-08-18 14:35:53 +0800

    Add files via upload

---
+++ basicexercisespart1/Problem11.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basicexercisespart1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Problem11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter Date of Birth: ");
+        int date = sc.nextInt();
+        System.out.println("Enter Address: ");
+         String address   = sc.nextLine();
+        System.out.println("Enter Age: ");
+        int age = sc.nextInt();
+        
+    }
+}
